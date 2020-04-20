bfb66d07ba88ecbb52c3582b742bb8e07b467d84
Glitch (hello-webpage) <none>
2020-04-20 04:24:16 +0000

    👖🎷 Checkpoint ./script.js:10805947/1477 ./index.html:10805947/6

--- script.js
+++ script.js
@@ -7,14 +7,14 @@ let textDiv= document.querySelector("#btn1")
 console.log(textDiv)
 
 textDiv.addEventListener("click", ()=>{
-  textDiv.innerHTML="you clicked me!!!"
+  textDiv.innerHTML="WASH HANDS!!!"
   
 });
 let distanceDiv= document.querySelector("#money2")
 console.log(distanceDiv)
 
 distanceDiv.addEventListener("click", ()=>{
-  distanceDiv.innerHTML="stay away"
+  distanceDiv.innerHTML="STAY AWAY!"
   
 });
 let facDiv= document.querySelector("#cake3")
@@ -23,4 +23,18 @@ console.log(facDiv)
 facDiv.addEventListener("click", ()=>{
 facDiv.innerHTML="don't touch it"
   
+});
+var settings = {
+	"async": true,
+	"crossDomain": true,
+	"url": "https://covid-19-data.p.rapidapi.com/help/countries?format=json",
+	"method": "GET",
+	"headers": {
+		"x-rapidapi-host": "covid-19-data.p.rapidapi.com",
+		"x-rapidapi-key": "SIGN-UP-FOR-KEY"
+	}
+}
+
+$.ajax(settings).done(function (response) {
+	console.log(response);
 });
